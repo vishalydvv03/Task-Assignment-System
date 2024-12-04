1839ba2e7a7706e8054fc4e39549d92f14cef483
Vishal Yadav <vishaljyadav576@gmail.com>
2024-12-04 23:13:25 +0530

    Minor Changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const express = require("express");
 const app = express();
@@ -13,7 +13,7 @@ app.use(methodOverride("_method"));
 
 const port = 8080;
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -22,115 +22,118 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
-app.listen(port, ()=>{
-    console.log(`Server listening on Port : ${port}`);
+app.listen(port, () => {
+  console.log(`Server listening on Port : ${port}`);
 });
 
-app.get("/task", (req,res)=>{
-    res.render("index.ejs");
+app.get("/task", (req, res) => {
+  res.render("index.ejs");
 });
 
-app.get("/task/add", (req,res)=>{
-    res.render("add.ejs");
+app.get("/task/add", (req, res) => {
+  res.render("add.ejs");
 });
 
-app.post("/task/add", (req,res)=>{
-    let {title,assignee,description,date,priority}=req.body;
-    let q = `INSERT INTO tasks (title,description,assignee,date,priority) VALUES (?,?,?,?,?)`;
-    try{
-        connection.query(q,[title,description,assignee,date,priority], (err,result)=>{
-            if(err){
-                throw err;
-            }
-            else{
-                res.redirect("/task/list");
-            }
-        });
-    }catch(err){
-        console.log(err);
-    }
-    
+app.post("/task/add", (req, res) => {
+  let { title, assignee, description, date, priority } = req.body;
+  let q = `INSERT INTO tasks (title,description,assignee,date,priority) VALUES (?,?,?,?,?)`;
+  try {
+    connection.query(
+      q,
+      [title, description, assignee, date, priority],
+      (err, result) => {
+        if (err) {
+          throw err;
+        } else {
+          res.redirect("/task/list");
+        }
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.get("/task/list",(req,res)=>{
-    let q =`SELECT * FROM tasks`;
-    try {
-        connection.query(q, (err,result)=>{
-            if(err) throw err;
-            else{
-                const tasks=result;
-                res.render("task-list.ejs", {tasks});
-            }
-        });
-    } catch(err) {
-        res.send(err);
-    }
+app.get("/task/list", (req, res) => {
+  let q = `SELECT * FROM tasks`;
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      else {
+        const tasks = result;
+        res.render("task-list.ejs", { tasks });
+      }
+    });
+  } catch (err) {
+    res.send(err);
+  }
 });
 
-app.get("/task/:id/details", (req,res)=>{
-    let {id} = req.params;
-    let q = `SELECT * FROM tasks WHERE id=?`;
-    try{
-        connection.query(q,[id],(err,result)=>{
-            if(err) throw err;
-            else{
-                res.render("details.ejs", {task: result[0]});
-            }
-        });
-    }catch(err){
-        res.send(err);
-    }
+app.get("/task/:id/details", (req, res) => {
+  let { id } = req.params;
+  let q = `SELECT * FROM tasks WHERE id=?`;
+  try {
+    connection.query(q, [id], (err, result) => {
+      if (err) throw err;
+      else {
+        res.render("details.ejs", { task: result[0] });
+      }
+    });
+  } catch (err) {
+    res.send(err);
+  }
 });
 
-app.get("/task/:id/edit", (req,res)=>{
-    let {id}= req.params;
-    let q = `SELECT * FROM tasks WHERE id=?`;
-    try{
-        connection.query(q,[id],(err,result)=>{
-            if(err) throw err;
-            else{
-                res.render("edit.ejs", {task: result[0]});
-            }
-        });
-    }catch(err){
-        res.send(err);
-    }
-
+app.get("/task/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let q = `SELECT * FROM tasks WHERE id=?`;
+  try {
+    connection.query(q, [id], (err, result) => {
+      if (err) throw err;
+      else {
+        res.render("edit.ejs", { task: result[0] });
+      }
+    });
+  } catch (err) {
+    res.send(err);
+  }
 });
 
-app.patch("/task/:id/edit", (req,res)=>{
-    let {id}= req.params;
-    let {title,description,assignee,date,priority} = req.body;
-    let q = `UPDATE tasks SET title=?, description =?, assignee=?, date=?, priority=? WHERE id=?`;
-    try{
-        connection.query(q,[title,description,assignee,date,priority,id],(err,result)=>{
-            if(err) throw err;
-            else{
-               res.redirect('/task/:id/details');
-            }
-        });
-    }catch(err){
-        res.send(err);
-    }
+app.patch("/task/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let { title, description, assignee, date, priority } = req.body;
+  let q = `UPDATE tasks SET title=?, description =?, assignee=?, date=?, priority=? WHERE id=?`;
+  try {
+    connection.query(
+      q,
+      [title, description, assignee, date, priority, id],
+      (err, result) => {
+        if (err) throw err;
+        else {
+          res.redirect("/task/:id/details");
+        }
+      }
+    );
+  } catch (err) {
+    res.send(err);
+  }
 });
 
-app.delete("/task/:id/delete", (req,res)=>{
-    let {id} = req.params;
-    let q = `DELETE FROM tasks where id =?`;
-    try{
-        connection.query(q,[id],(err,result)=>{
-            if(err) throw err;
-            else{
-                res.redirect("/task/list");
-            }
-        });
-    }catch(err){
-
-    }
+app.delete("/task/:id/delete", (req, res) => {
+  let { id } = req.params;
+  let q = `DELETE FROM tasks where id =?`;
+  try {
+    connection.query(q, [id], (err, result) => {
+      if (err) throw err;
+      else {
+        res.redirect("/task/list");
+      }
+    });
+  } catch (err) {}
 });
